bff252b201b50bfd2611e594bd7a13a33656a978
Irfan K <unknown7k7k@gmail.com>
2024-07-27 09:02:32 +0530

    updated

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,15 +1,10 @@
 import About from "@/components/About";
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import HomePageLoader from "@/components/Loader/HomepageLoader";
 import Project from "@/components/Project";
 import ProjectHorizontalScroll from "@/components/ProjectHorizontalScroll";
 import DesktopBreakPoint from "@/components/ResponsiveUtilities/DesktopBreakPoint";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const HomePage = () => {
-  gsap.registerPlugin(ScrollTrigger);
   return (
     <div>
       <div className="w-screen h-screen block b "></div>
